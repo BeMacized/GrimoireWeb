f16fa933226652fc120d3554bd33b40337ca10d5
Bodhi Mulders <info@bemacized.net>
2017-08-18 23:20:33 +0200

    Updated Support server URL

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,5 @@
 // @flow
 export const INVITE_URL = 'https://discordapp.com/oauth2/authorize?client_id=239399098862665728&scope=bot&permissions=314432'
-export const SUPPORT_INVITE_URL = 'https://discord.gg/F8c3jhm'
+export const SUPPORT_INVITE_URL = 'https://discord.gg/QtHRrxh'
 export const GITHUB_MAIN_REPO = 'https://github.com/BeMacized/Grimoire'
 export const GITHUB_WEB_REPO = 'https://github.com/BeMacized/GrimoireWeb'
